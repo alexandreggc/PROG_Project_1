395dc844999ca539b0d9e8d0c677ebeca87f417e
alexandreggc <correiaalexandrealex@gmail.com>
2021-04-25 21:33:13 +0100

    gameloop, player movement and minimum path vector

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <ios>
+#include <limits>
 #include <iomanip>
 #include <sstream>
 #include <fstream>
@@ -13,6 +15,8 @@ struct Robot {
 struct Player {
     int x, y;
 };
+
+
 
 void invalidInput();
 void rules();
@@ -34,6 +38,7 @@ int main() {
             if (cin.peek() == '\n' && !cin.fail() && (opt == 0 || opt == 1 || opt == 2)) {
                 break;
             }
+            else if (cin.fail() && cin.eof()) exit(0);
             invalidInput();
         }
 
@@ -59,6 +64,18 @@ void invalidInput() {
     cout << "Please choose a valid option." << endl;
 }
 
+vector<int> move_dir(char dir) {
+    if (dir == 'Q') return { -1,-1 };
+    if (dir == 'W') return { 0,-1 };
+    if (dir == 'E') return { 1,-1 };
+    if (dir == 'A') return { -1,0 };
+    if (dir == 'S') return { 0,0 };
+    if (dir == 'D') return { 1,0 };
+    if (dir == 'Z') return { -1,1 };
+    if (dir == 'X') return { 0,1 };
+    if (dir == 'C') return { 1,1 };
+}
+
 void rules() {
     int comeback = -1;
     cout << endl;
@@ -70,6 +87,7 @@ void rules() {
         if (cin.peek() == '\n' && !cin.fail() && comeback == 0) {
             break;
         }
+        else if (cin.fail() && cin.eof()) exit(0);
         invalidInput();
     }
 }
@@ -80,12 +98,13 @@ string choose_maze() {
     string filename;
     stringstream s;   
     cout << endl;
-    cout << "Insert maze's number (1-99) or 0 to return to main menu: ";
+    cout << "Enter the maze number (1-99) or 0 to return to main menu: ";
     while (true) {
         cin >> maze_number;
         if (cin.peek() == '\n' && !cin.fail() && maze_number >= 0 && maze_number < 100) {
             break;
         }
+        else if (cin.fail() && cin.eof()) exit(0);
         invalidInput();
     }
     if (maze_number == 0)
@@ -125,6 +144,7 @@ void maze_to_vectors(ifstream &f, vector<vector<char>> &maze) {
     }
 }
 
+// function that identify the player and the robots in the maze
 void identify_elements(vector<vector<char>> &maze, Player &pl, vector<Robot> &rb) {
     int count = 0;
     for (size_t i = 0; i < maze.size(); i++) {
@@ -138,8 +158,85 @@ void identify_elements(vector<vector<char>> &maze, Player &pl, vector<Robot> &rb
                 pl = { (int)j, (int)i };
         }
     }
-    /*cout << rb[0].x << endl << rb[1].x << endl;
-    cout << pl.x << " " << pl.y << endl;*/
+    cout << rb[0].x << endl << rb[0].y << endl;
+    cout << pl.x << " " << pl.y << endl;
+}
+
+// function that return the direction of the minimum path from robot to player
+vector<int> min_path(Player& pl, Robot& rb) {
+    if (rb.x > pl.x)
+        if (rb.y > pl.y)
+            return { -1, -1 };
+    else if (rb.x < pl.x)
+        if (rb.y > pl.y)
+            return { 1, -1 };
+    else if (rb.x > pl.x)
+        if (rb.y < pl.y)
+            return { -1, 1 };
+    else if (rb.x < pl.x)
+        if (rb.y < pl.y)
+            return { 1, 1 };
+    else if (rb.x == pl.x)
+        if (rb.y > pl.y)
+            return { 0, -1 };
+        if (rb.y < pl.y)
+            return { 0, 1 };
+    else if (rb.y == pl.y)
+        if (rb.x > pl.x)
+            return { -1, 0 };
+        if (rb.x < pl.x)
+            return { 1, 0 };
+}
+
+void display_maze(vector<vector<char>>& maze) {
+    for (size_t i = 0; i < maze.size(); i++) {
+        cout << endl;
+        for (size_t j = 0; j < maze.at(i).size(); j++) {
+            cout << maze.at(i).at(j);
+        }
+    }
+}
+
+void gameover() {
+    exit(0);
+}
+
+int update_player_pos(vector<vector<char>>& maze, Player& pl, char dir) {
+    int x, y;
+    x = move_dir(dir).at(0) + pl.x;
+    y = move_dir(dir).at(1) + pl.y;
+    char maze_c = maze.at(y).at(x);
+    if (maze_c == 'r')
+        return 0;
+    if (maze_c == ' ') {
+        maze.at(pl.y).at(pl.x) = ' ';
+        maze.at(y).at(x) = 'H';
+        pl.x = x;
+        pl.y = y;
+        return 1;
+    }
+    if (maze_c == 'R' || maze_c == '*')
+        gameover();
+
+}
+
+// function that returns a vetor with the direction of the player's move
+void player_input(vector<vector<char>>& maze, Player &pl) {
+    char dir;
+    cout << endl;
+    cout << "Enter the position you want to move to: ";
+    while (true) {
+        cin >> dir;
+        dir = toupper(dir);
+        if (cin.peek() == '\n' && !cin.fail() && (dir == 'Q' || dir == 'W' || dir == 'A' || 
+            dir == 'S' || dir == 'Z' || dir == 'D' || dir == 'X' || dir == 'C' || dir == 'E')) 
+        {
+            if (update_player_pos(maze, pl, dir))
+                break;
+        }
+        else if (cin.fail() && cin.eof()) exit(0);
+        invalidInput();
+    }
 }
 
 void play() {
@@ -149,18 +246,21 @@ void play() {
 
     Player player;
     vector<Robot> robots;
+    vector<int> player_dir;
 
+    // prepare data to initialize the game
     if (!load_mazefile(f))
         return;
     maze_to_vectors(f, maze);
     identify_elements(maze, player, robots);
 
-    // display maze
-    for (size_t i = 0; i < maze.size(); i++) {
-        cout << endl;
-        for (size_t j = 0; j < maze.at(i).size(); j++) {
-            cout << maze.at(i).at(j);
-        }
+    // game loop
+    while (true) {
+        display_maze(maze);
+        player_input(maze, player);
+        /*for (size_t i = 0; i < robots.size(); i++) {
+
+        }*/
     }
 }
 
